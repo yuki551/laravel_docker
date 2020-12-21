b5b742d8e4206ffb84d074dc8486c2b53f7db9ac
kota_ <kota.sozosha@gmail.com>
2020-12-21 23:19:13 +0900

    FullCalendarのjs追加

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -16,11 +16,11 @@ Vue.use(PortalVue);
 const app = document.getElementById('app');
 
 new Vue({
-    render: (h) =>
+    render: h =>
         h(InertiaApp, {
             props: {
                 initialPage: JSON.parse(app.dataset.page),
-                resolveComponent: (name) => require(`./Pages/${name}`).default,
+                resolveComponent: name => require(`./Pages/${name}`).default,
             },
         }),
 }).$mount(app);
