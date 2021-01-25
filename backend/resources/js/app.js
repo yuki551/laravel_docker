3e75d4a487dc20df324df0a5ec009744dc38f1d7
yuki551 <nomurayuki.sozosha@gmail.com>
2021-01-25 11:59:10 +0900

    日付整形vueMoment追加

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -7,11 +7,13 @@ import Vue from 'vue';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import VueMoment from 'vue-moment';
 
 Vue.mixin({ methods: { route } });
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(VueMoment);
 
 const app = document.getElementById('app');
 
