1a9a02dbe5bd397ca0e9b71aa69142d3266badfe
kota623 <kota.sozosha@gmail.com>
2021-01-29 15:23:00 +0900

    auth日本語化

--- backend/resources/views/auth/reset-password.blade.php
+++ backend/resources/views/auth/reset-password.blade.php
@@ -4,31 +4,29 @@
             <x-jet-authentication-card-logo />
         </x-slot>
 
+        <div class="mb-4 text-sm text-gray-600">
+            パスワードを忘れた場合は、以下に登録メールアドレスを入力して送信してください。パスワード再発行リンクを送信します。
+        </div>
+
+        @if (session('status'))
+        <div class="mb-4 font-medium text-sm text-green-600">
+            {{ session('status') }}
+        </div>
+        @endif
+
         <x-jet-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.update') }}">
+        <form method="POST" action="/forgot-password">
             @csrf
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
-
             <div class="block">
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+                <x-jet-label value="メールアドレス" />
+                <x-jet-input class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Reset Password') }}
+                    パスワード再発行リンクを送信する
                 </x-jet-button>
             </div>
         </form>
